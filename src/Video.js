385d256a02f7e9f878807bd8e6d214528135a130
Leila Baneshi <baneshi.leila@gmail.com>
2020-12-11 16:21:50 +0100

    set state for paly and pause the video

--- src/Video.js
+++ src/Video.js
@@ -1,11 +1,18 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './Video.css';
 
 function Video() {
+    const [playing, setPlaying] = useState(false)
     const videoRef = useRef(null);
 
     const onVideoPress = () => {
-
+        if (playing) {
+            videoRef.current.pause();
+            setPlaying(false);
+        } else {
+            videoRef.current.play();
+            setPlaying(true);
+        }
     }
 
     return (
